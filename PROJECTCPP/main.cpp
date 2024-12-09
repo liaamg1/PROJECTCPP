53408d311202932269478132818b48717ed2810a
liaamg1 <lgustafsson71@gmail.com>
2024-12-09 12:53:44 +0100

    Clean up

--- PROJECTCPP/main.cpp
+++ PROJECTCPP/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
-#include "Goods.h"
+
 int main() {
 	std::cout << "Hello World!" << std::endl; // TEST 1
-	Goods goods1(500, 2, "Koppar", "EU");
-	std::cout << goods1.getType() << std::endl;
+	
 	return 0;
 }

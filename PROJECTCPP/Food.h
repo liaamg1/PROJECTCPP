d5faac954440c9bb516b3bf42ddd6b91f2a408af
mansbackstrom <mans.backstrom@hotmail.se>
2024-12-17 15:05:50 +0100

    tse

--- PROJECTCPP/Food.h
+++ PROJECTCPP/Food.h
@@ -10,6 +10,7 @@ private:
 public:
     Food(int quantity = 0, float weight = 0.0f, const std::string& type = "Food", const std::string& name = "Unknown");  // Initialisera quantity till 0
     ~Food(); // Destruktor
+    void displayInfo() const;
     void setQuantity(int quantity);  // Setter för quantity
     int getQuantity() const override;  // Getter för quantity
     std::string toString() const;

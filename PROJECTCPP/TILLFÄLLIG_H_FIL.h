4c7aa2f34d00a28dc27512d52c23538188e690b4
liaamg1 <lgustafsson71@gmail.com>
2025-01-08 15:26:30 +0100

    .

--- PROJECTCPP/TILLFÄLLIG_H_FIL.h
+++ PROJECTCPP/TILLFÄLLIG_H_FIL.h
@@ -6,8 +6,14 @@
 //Användande av undantagshantering där detta är lämpligt = 1p
 //Grafiskt användargränssnitt(ex - vis SFML) = förhoppnigsvis mer än 2p
 //Relevant användande av funktionspekare : 1 poäng - FIXAT TROLIGTVIS
+//Relevant användande av Lamdautryck : 1 poäng - FIXAT TROLIGTVIS
 
 //---------------KVAR ATT FIXA------------------------
 //Egendefinerad rimlig generisk klass som används på ett relevant sätt : 2 poäng
-//Relevant användande av Lamdautryck : 1 poäng - på gång
+
 //Läsning från och/eller skrivning till textfiler VIKTIG KRAV - på gång
+
+
+
+
+//************* Buggen kring fel namn loop = oändlig **************
